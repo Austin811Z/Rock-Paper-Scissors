7d1237ebb17e748d1cb80889b7d001c6a547d863
Austin811Z <Sbonisomkhize134@gmail.com>
2026-02-05 07:27:17 +0200

    Added function to play an entire round.

--- script.js
+++ script.js
@@ -28,5 +28,71 @@ function getHumanChoice()
     let choice = prompt('Enter Rock/Paper/Scissors: ');
     return choice;
 }
-let comC = getComputerChoice();
-let humC = getHumanChoice();
+function playRound(comC,humC)
+{
+    let com = comC
+    let hum = humC
+    let msg = '';
+    switch (hum)
+    {
+        case 'rock':
+            if (com == 'rock') 
+            {
+                msg = 'Draw';
+            }
+            else if (com == 'Paper')
+            {
+                msg = 'Paper beats rock : computer';
+                computerScore = computerScore + 1;
+            }
+            else 
+            {
+                msg = 'Rock beats scissiors : human';
+                humanSCore = humanSCore + 1;
+            }
+            break;
+        case 'paper':
+            if (com == 'paper') 
+            {
+                msg = 'Draw';
+            }
+            else if (com == 'rock')
+            {
+                msg = 'Paper beats rock : human';
+                humanSCore = humanSCore + 1;
+            }
+            else 
+            {
+                msg = 'Scissors beats Paper : computer ';
+                computerScore = computerScore + 1;
+            }
+            break;
+        case 'scissors':
+            if (com == 'scissors') 
+            {
+                msg = 'Draw';
+            }
+            else if (com == 'Paper')
+            {
+                msg = 'Scissors beats paper: human';
+                humanSCore = humanSCore + 1;
+            }
+            else 
+            {
+                msg = 'Rock beats scissiors: computer';
+                computerScore = computerScore + 1;
+            }
+            break;
+    }
+    return msg;
+
+
+}
+const x = getComputerChoice();
+let y = getHumanChoice();
+let comC = x.toLowerCase();
+let humC = y.toLowerCase();
+let play = playRound(comC, humC);
+console.log(play);
+console.log(humanSCore);
+console.log(computerScore);
